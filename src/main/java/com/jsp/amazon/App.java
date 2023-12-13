5826c10f312f899a1413f6fff3104331fcf4c7c1
rishav2908 <19BCS2527@gmail.com>
2023-12-13 14:06:12 +0530

    changed one

---
+++ src/main/java/com/jsp/amazon/App.java
@@ -0,0 +1,15 @@
+package com.jsp.amazon;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        //changed one made to master
+        // change one made to
+    }
+}
